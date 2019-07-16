e55505909a01d5dcc449d8067e5a8bb387ec5ffc
Rana-akbar <rana.akbar.08@gmail.com>
2019-07-16 19:12:54 +0100

    -complete

--- start_point/controller.rb
+++ start_point/controller.rb
@@ -1,10 +1,11 @@
 require ("sinatra")
-require ("sinatra_reloader") if developer?
+require ("sinatra/reloader") if development?
 require_relative('./models/rps.rb')
 also_reload("./models/*")
 
-get 'rps/:player1/:player2' do
-  hand1 = [:player1]
-  hand2 = [:player2]
-  @winner = RPSGame.new(hand1, hand2)
+get '/rps/:player1/:player2' do
+  hand1 = params[:player1].to_s
+  hand2 = params[:player2].to_s
+  @winner = RPSGame.check_win(hand1, hand2)
+  erb(:result)
 end
